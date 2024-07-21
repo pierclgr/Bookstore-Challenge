ca43381dfc2af35a7aeb379c3d4a39da5889c2d9
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2024-07-21 17:32:19 +0200

    Added main features: list and add book

---
+++ backend/books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_year = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True) 
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+
